bf81d7c4fce66fae5f9418aace6c8a57e72d4a78
rpschedule <dallin.reeser779@raypec.org>
2023-02-14 09:17:29 -0600

    added board and squares

---
+++ first-react-app/src/Board.js
@@ -0,0 +1,13 @@
+import Square from "./Square";
+
+function Board () {
+    return (
+        <>
+            <h1>im a board</h1>
+            <p>pls love me</p>
+            <Square />
+        </>
+    );
+}
+
+export default Board;
---
+++ first-react-app/src/Square.js
@@ -0,0 +1,10 @@
+function Square () {
+    return (
+        <>
+            <h2>Im a square</h2>
+            <p>you dont have to love me but i'd appriciate it if you did</p>
+        </>
+    )
+}
+
+export default Square;
