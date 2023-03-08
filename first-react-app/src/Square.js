49d1e4c89d7582f34c99c860709caea9733b5d10
rpschedule <dallin.reeser779@raypec.org>
2023-03-08 09:07:34 -0600

    updated functionality to work with pngs

--- first-react-app/src/Square.js
+++ first-react-app/src/Square.js
@@ -1,15 +1,61 @@
 import './Square.css'
+const images = importAll(require.context('./pieces/', true, /\.(png|jpe?g|svg)$/));
 
-export default function Square ({color, piece}) {
-    let svg;
+function importAll(r) {
+    return r.keys().map(r);
+}
 
-    switch ( piece ) {
-        
+export default function Square({ color, piece }) {
+    let pieceIndex;
+
+    switch (piece) {
+        case 'b':
+            pieceIndex = 0;
+            break;
+        case 'k':
+            pieceIndex = 1;
+            break;
+        case 'n':
+            pieceIndex = 2;
+            break;
+        case 'p':
+            pieceIndex = 3;
+            break;
+        case 'q':
+            pieceIndex = 4;
+            break;
+        case 'r':
+            pieceIndex = 5;
+            break;
+        case 'B':
+            pieceIndex = 6;
+            break;
+        case 'K':
+            pieceIndex = 7;
+            break;
+        case 'N':
+            pieceIndex = 8;
+            break;
+        case 'P':
+            pieceIndex = 9;
+            break;
+        case 'Q':
+            pieceIndex = 10;
+            break;
+        case 'R':
+            pieceIndex = 11;
+            break;
+        default:
+            break;
     }
-    
+
     return (
         <div className={`Square ${color}`}>
-            {svg}
+            <img
+                src={images[pieceIndex]}
+                alt={piece}
+                className="Piece"
+            />
         </div>
     )
 }
