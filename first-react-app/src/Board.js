1e986cfb730a7e9037e107401e99eff0d0d2c93b
Grom <grom@kali>
2023-03-09 20:39:58 -0700

    todo: find replacement for e.target.piece

--- first-react-app/src/Board.js
+++ first-react-app/src/Board.js
@@ -2,29 +2,35 @@ import Square from "./Square.js";
 import './Board.css'
 import loadFen from "./utils/loadFen.js";
 import move from './utils/move.js'
+import { useState } from 'react';
 
 export default function Board () {
+    const [prevMove, setPrevMove] = useState(-1) 
     let squares = Array(64).fill('');
+
+    function handleClick (e) {
+        setPrevMove(e.target);
+        console.log(prevMove);
+    }
 
     squares = loadFen('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0');
     squares = move(squares, 0, 34)
     squares = move(squares, 34, 16)
 
-    return ( renderBoard(squares) );
+    return ( renderBoard(squares, handleClick) );
 }
 
-function renderBoard (board) {
+function renderBoard (board, handleClick) {
     const boardRendered = board.map((piece, index) => {
         // if it's an odd row, offset index by 1
 
         const offset = Math.floor(index / 8) % 2;
-        console.log(offset)
 
         let color;
         if ( (index + offset) % 2 === 0 ) { color = 'White'; }
         else { color = 'Black'; }
 
-        return <Square key={index} color={color} piece={piece} onclick=""/>
+        return <Square key={index} color={color} piece={piece} clickHandler={handleClick}/>
     })
     
 
