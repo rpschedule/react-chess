2a92b6dd49f22bb72801cda935c08d8e2b725038
rpschedule <dallin.reeser779@raypec.org>
2023-03-17 09:07:14 -0500

    TODO: make legalMoves return array of objects

--- src/utils/move.js
+++ src/utils/move.js
@@ -1,21 +1,26 @@
-import isLegal from './isLegal.js'
+import getLegalMoves from './legalMoves.js'
 
 export default function move(board, origin, destination) {
     if ( origin === destination ) return board;
 
     let out = board; // out = [{piece: r, ...}, {piece: k, ...}, . . . ]
+    const legalMoves = getLegalMoves(board, origin, destination)
 
     // prevents players from deleting pieces
-    if ( board[origin].piece !== '' && isLegal(board, origin, destination)) { 
-        board[destination].piece = board[origin].piece;
-        board[destination].pieceColor = board[origin].pieceColor; 
+    if ( board[origin].piece !== '' && legalMoves.moves.includes(destination)) { 
+        board = board.map((square) => Object.assign(square, { 
+            pawnJustMovedTwice: false,
+        }))
 
-        board = board.map((square) => Object.assign(square, { pawnJustMovedTwice: false }))
+        board[destination] = Object.assign(board[destination], {
+            piece: board[origin].piece,
+            pieceColor: board[origin].pieceColor,
+            pawnJustMovedTwice: (origin - 16 === destination || origin + 16 === destination) && board[origin].piece === 'p',
+        })
 
         board[origin] = Object.assign(board[origin], {
             piece: '',
             pieceColor: '',
-            pawnJustMovedTwice: (origin - 16 === destination || origin + 16 === destination) && board[origin].piece === 'p' ? true : false,
         });
     }
     
