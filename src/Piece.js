756e24339926771fb4dfcc5763b5a91ef19d28b4
rpschedule <103065303+rpschedule@users.noreply.github.com>
2023-04-20 18:33:23 +0000

    drag and drop

--- src/Piece.js
+++ src/Piece.js
@@ -1,4 +1,4 @@
-import { useDrag } from 'react-dnd'
+import { useDrag, DragPreviewImage } from 'react-dnd'
 
 import './Piece.css'
 
@@ -15,21 +15,22 @@ import whiteb from './images/pieces/white/B.png'
 import whitep from './images/pieces/white/P.png'
 import whitek from './images/pieces/white/K.png'
 
-export default function Piece (square) {
-    const image = (square.piece === 'b' && square.pieceColor === 'black') ? blackb :
-        (square.piece === 'k' && square.pieceColor === 'black') ? blackk :
-        (square.piece === 'n' && square.pieceColor === 'black') ? blackn :
-        (square.piece === 'p' && square.pieceColor === 'black') ? blackp :
-        (square.piece === 'q' && square.pieceColor === 'black') ? blackq :
-        (square.piece === 'r' && square.pieceColor === 'black') ? 5 :
-        (square.piece === 'b' && square.pieceColor === 'white') ? 6 :
-        (square.piece === 'k' && square.pieceColor === 'white') ? 7 :
-        (square.piece === 'n' && square.pieceColor === 'white') ? 8 :
-        (square.piece === 'p' && square.pieceColor === 'white') ? 9 :
-        (square.piece === 'q' && square.pieceColor === 'white') ? 10 :
-        (square.piece === 'r' && square.pieceColor === 'white') ? 11 : undefined;
+export default function Piece ({ square }) {
+    const image = (square.piece === 'b' && square.pieceColor === 'black') ? blackb 
+        : (square.piece === 'k' && square.pieceColor === 'black') ? blackk
+        : (square.piece === 'n' && square.pieceColor === 'black') ? blackn
+        : (square.piece === 'p' && square.pieceColor === 'black') ? blackp
+        : (square.piece === 'q' && square.pieceColor === 'black') ? blackq
+        : (square.piece === 'r' && square.pieceColor === 'black') ? blackr
+        : (square.piece === 'b' && square.pieceColor === 'white') ? whiteb
+        : (square.piece === 'k' && square.pieceColor === 'white') ? whitek
+        : (square.piece === 'n' && square.pieceColor === 'white') ? whiten
+        : (square.piece === 'p' && square.pieceColor === 'white') ? whitep
+        : (square.piece === 'q' && square.pieceColor === 'white') ? whiteq
+        : (square.piece === 'r' && square.pieceColor === 'white') ? whiter
+        : undefined;
 
-    const [{isDragging}, drag] = useDrag(() => ({
+    const [{isDragging}, drag, preview] = useDrag(() => ({
         type: 'piece',
         e: square.piece,
         collect: monitor => ({
@@ -38,6 +39,11 @@ export default function Piece (square) {
     }))
 
     return (
-        (pieceIndex !== undefined) ? <img ref={drag} src={images[pieceIndex]} alt={square.piece} className="Piece" /> : ''
+        
+        (image !== undefined) ? 
+        <>
+            <DragPreviewImage connect={preview} src={image} />
+            <img ref={drag} src={image} alt={square.piece} className="Piece" />
+        </> : ''
     );
 }
